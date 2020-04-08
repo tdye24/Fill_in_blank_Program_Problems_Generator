a1b3336b4f9e48bd2c04c11c2cfb4780d4b2ff08
TDYe123 <18365225454@163.com>
2020-04-08 20:21:17 +0800

    amend: (1) change multi threads to multi processes using threading pool(default 4)

--- judge/judge_client_2.py
+++ judge/judge_client_2.py
@@ -1,7 +1,7 @@
 # ！usr/bin/python
 # -*- coding:utf-8 -*-#
-# @date:2020/4/5 10:15
-# @name:tcp_socket_client
+# @date:2020/4/5 11:08
+# @name:tcp_socket_client_1
 # @author:TDYe
 import socket
 import subprocess
