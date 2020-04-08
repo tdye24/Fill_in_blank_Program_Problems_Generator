08d326efb739f47742f9133112ee8636ac7ec514
TDYe123 <18365225454@163.com>
2020-04-08 10:11:41 +0800

    add: implement two functions (1) count the number of test cases corresponding to specified problem (2) add Universal header file <bits/stdc++.h> and insert io redirect code to original submission code to generate normal code, which is named like 10001-1001-1-normal-1, 10001 means submission id, 1001 means problem id, 1 means the first combination of user answer and correct answer with elp problem code, normal means it has been transformed to normal form which can load test cases and be compile to generate executable file, 1 means this normal file loads the first test case corresponding to the problem from the disk to judge whether it is right on the first test case.

---
+++ judge/tools.py
@@ -0,0 +1,48 @@
+# ！usr/bin/python
+# -*- coding:utf-8 -*-#
+# @date:2020/4/6 21:34
+# @name:utils
+# @author:TDYe
+import re
+import os
+
+
+def get_test_cases_num(proId: str):
+	"""
+	given a proId then get test cases num according to this problem
+	:param proId: path to the problem
+	:return: num of test cases
+	"""
+	count = 0
+	path = '../data/test_cases/%s' % proId
+	for _ in os.listdir(path):
+		count = count + 1
+	return int(count / 2)
+
+
+def io_redirect(submissionId_proId_th_: str, i: str, o: str):
+	content = ''
+	problem_id = submissionId_proId_th_.split('-')[1]
+	in_path = '../test_cases/%s/%s.in' % (problem_id, i)
+	out_path = '../out/%s-%s.out' % (submissionId_proId_th_, o)
+	path = '../data/submissions/%s.cpp' % submissionId_proId_th_
+	try:
+		f = open(file=path, mode='r', encoding='utf-8')
+		for line in f.readlines():
+			content += line
+	except IOError:
+		print("reading file failed")
+	finally:
+		f.close()
+		# add include file #include<bits/stdc++.h> and IO redirect
+		content = '#include<bits/stdc++.h>\nusing namespace std;\n' + content
+		content = re.sub(r'main\s*\(.*?\)\s*{', "main() {\n    freopen(\"%s\",\"r\",stdin);\n    freopen(\"%s\",\"w\",stdout);" % (in_path, out_path), content)
+		with open('../data/submissions/%s-normal-%s.cpp' % (submissionId_proId_th_, o), 'w') as f1:
+			f1.write(content)
+
+
+def generate_normal_files(submissionId_proId_th_):
+	problem_id = submissionId_proId_th_.split('-')[1]    # str
+	test_cases_num = get_test_cases_num(problem_id)
+	for i in range(test_cases_num):
+		io_redirect(submissionId_proId_th_, str(i+1), str(i+1))
