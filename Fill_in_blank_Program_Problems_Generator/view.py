3e55679ba35474fd1b5a4c089949e2e1b1b7c371
TDYe123 <18365225454@163.com>
2020-04-19 01:11:41 +0800

    fix: (1) move some function code to models' static method area
    add:
    (1) teacher action routes handler including signin logout and uploading problem file

--- Fill_in_blank_Program_Problems_Generator/view.py
+++ Fill_in_blank_Program_Problems_Generator/view.py
@@ -3,12 +3,14 @@
 # @date:2020/3/13 20:44
 # @name:view
 # @author:TDYe
+import json
+import os
+from .tools import unzip_file
 from django.core import serializers
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
-from DBModel.models import User, Problem, Submission
+from DBModel.models import User, Problem, Submission, Teacher
 from django.contrib.auth.hashers import make_password, check_password
-import json
 
 
 def index(request):
@@ -17,7 +19,8 @@ def index(request):
 	:param request:
 	:return:
 	"""
-	problemList, problemVolumes = _get_problem_list(1)
+	problemList = Problem.get_problem_list(volume=1)
+	problemVolumes = Problem.get_problem_volumes()
 	return render(request, 'index.html', {
 								'problemList': problemList,
 								'problemVolume': 1,
@@ -41,7 +44,7 @@ def user(request):
 			email = request.POST.get('email')
 			password = request.POST.get('password')
 			exist = User.objects.filter(email=email).exists()
-			next = request.GET.get('next')
+			nextURL = request.GET.get('next')
 			if exist:
 				user = User.objects.all().filter(email=email)[0]
 				if check_password(password, user.password):
@@ -49,13 +52,13 @@ def user(request):
 					request.session['email'] = user.email
 					request.session['nickname'] = user.nickname
 					request.session['score'] = str(user.score)
-					return redirect(next)
+					return redirect(nextURL)
 				else:
 					# The password is wrong.
-					return redirect(next)
+					return redirect(nextURL)
 			else:
 				# The account does not exist.
-				return render(next)
+				return redirect(nextURL)
 		elif request.GET.get('action') == 'signup':
 			email, password, nickname \
 				= request.POST.get('email'), request.POST.get('password'), request.POST.get('nickname')
@@ -68,8 +71,16 @@ def user(request):
 				return redirect(next)
 			except ValueError:
 				print("Invalid parameters => (email, password, nickname)")
-			else:
-				return render(request, 'register.html')
+		else:
+			return render(request, 'register.html')
+	elif request.method == 'GET':
+		if request.GET.get('action') == 'logout':
+			nextURL = request.GET.get('next')
+			request.session['checkSignin'] = False
+			request.session['email'] = ''
+			request.session['nickname'] = ''
+			request.session['score'] = ''
+			return redirect(nextURL)
 
 
 def register(request):
@@ -90,7 +101,8 @@ def get_problem_list(request):
 	:return:
 	"""
 	problemVolume = eval(request.GET.get('volume'))
-	problemList, problemVolumes = _get_problem_list(problemVolume)
+	problemList = Problem.get_problem_list(volume=problemVolume)
+	problemVolumes = Problem.get_problem_volumes()
 	return render(request, 'index.html', {'problemList': problemList,
 	                                      'problemVolume': problemVolume,
 	                                      'problemVolumes': problemVolumes,
@@ -102,13 +114,6 @@ def get_problem_list(request):
 	                                      'rankContentClass': 'hidden', })
 
 
-def _get_problem_list(volume):
-	problemList = Problem.objects.values('id', 'title', 'averageScore', 'score'). \
-		              order_by('id')[(volume - 1) * 20: volume * 20]
-	volumes = [(i + 1) for i in range(int(Problem.objects.count() / 20) + 1)]
-	return problemList, volumes
-
-
 def get_problem_by_id(request):
 	"""
 	get specified problem targeted by problem id
@@ -116,11 +121,7 @@ def get_problem_by_id(request):
 	:return:
 	"""
 	id = eval(request.GET.get('id'))
-	problem = Problem.objects.values(
-		'id', 'title', 'theme', 'author', 'description', 'answer', 'averageScore', 'score').filter(id=id)[0]
-	problem['theme'] = problem['theme'].split(',')
-	problem['blanksNums'] = [(i+1) for i in range(len(problem['answer'].split(',')))]
-	del(problem['answer'])
+	problem = Problem.get_problem_by_id(id=id)
 	return render(request, 'problem.html', {'problem': problem,
 	                                        'problemTabClass': 'active',
 	                                        'statusTabClass': '',
@@ -144,9 +145,8 @@ def get_problem_by_theme(request):
 	"""
 	theme = request.GET.get('theme')
 	problemVolume = eval(request.GET.get('volume'))
-	problemList = Problem.objects.filter(theme__contains=theme).values('id', 'title', 'averageScore', 'score'). \
-		order_by('id')[(problemVolume - 1) * 20: problemVolume * 20]
-	problemVolumes = [(i + 1) for i in range(int(Problem.objects.filter(theme__contains=theme).count() / 20) + 1)]
+	problemList = Problem.get_problem_list_by_theme(theme=theme, problemVolume=problemVolume)
+	problemVolumes = Problem.get_problem_volumes_by_theme(theme=theme)
 	return render(request, 'theme.html', {
 							'problemList': problemList,
 	                        'theme': theme,
@@ -167,8 +167,8 @@ def get_status_list(request):
 	:return:
 	"""
 	statusVolume = eval(request.GET.get('volume'))
-	statusList = Submission.objects.all().order_by('-runId')[(statusVolume - 1) * 20: statusVolume * 20]
-	statusVolumes = [(i + 1) for i in range(int(Submission.objects.count() / 20) + 1)]
+	statusList = Submission.get_status_list(statusVolume=statusVolume)
+	statusVolumes = Submission.get_status_volumes()
 	return render(request, 'index.html', {'statusList': statusList,
 	                                      'statusVolume': statusVolume,
 	                                      'statusVolumes': statusVolumes,
@@ -181,9 +181,14 @@ def get_status_list(request):
 
 
 def get_ranklist(request):
+	"""
+	get rank list
+	:param request:
+	:return:
+	"""
 	ranklistVolume = eval(request.GET.get('volume'))
-	ranklist = User.objects.all().order_by('-score')[(ranklistVolume - 1) * 20: ranklistVolume * 20]
-	ranklistVolumes = [(i + 1) for i in range(int(User.objects.count() / 20) + 1)]
+	ranklist = User.get_ranklist(ranklistVolume=ranklistVolume)
+	ranklistVolumes = User.get_ranklist_volumes()
 	return render(request, 'index.html', {'ranklist': ranklist,
 	                                      'ranklistVolume': ranklistVolume,
 	                                      'ranklistVolumes': ranklistVolumes,
@@ -195,9 +200,82 @@ def get_ranklist(request):
 	                                      'rankContentClass': '', })
 
 
-def upload_problem(request):
-	pass
-
-
 def submit(request):
 	pass
+
+
+def teacher_index(request):
+	return render(request, 'teacher_index.html')
+
+
+def teacher(request):
+	"""
+	teacher signup, signin, checkLogin/
+	:param request:
+	:return:
+	"""
+	if request.method == 'POST':
+		if request.GET.get('action') == 'signin':
+			email = request.POST.get('email')
+			password = request.POST.get('password')
+			exist = Teacher.objects.filter(email=email).exists()
+			nextURL = request.GET.get('next')
+			if exist:
+				teacher = Teacher.objects.all().filter(email=email)[0]
+				if check_password(password, teacher.password):
+					request.session['checkTeacherSignin'] = True
+					request.session['teacherEmail'] = teacher.email
+					request.session['teacherNickname'] = teacher.nickname
+					return redirect(nextURL)
+				else:
+					# The password is wrong.
+					return redirect(nextURL)
+			else:
+				# The account does not exist.
+				return render(nextURL)
+		elif request.GET.get('action') == 'upload':
+			title = request.POST.get('title')
+			themes = request.POST.get('themes')
+			description = request.POST.get('description')
+			print('title', title)
+			print('themes', themes)
+			print('description', description)
+			id = Problem.get_next_problem_id()
+			filename = "%s%s" % (str(id), '.cpp')
+			print('id', id)
+			problem_file_obj = request.FILES.get('problem-file')
+			problem_file_path = os.path.join('data/problem', filename)
+			f = open(problem_file_path, mode="wb")
+			for i in problem_file_obj.chunks():     # TODO(tdye): using coroutine?
+				f.write(i)
+			f.close()
+			test_cases_obj = request.FILES.get('test-cases')
+			if not os.path.exists("%s%s" % ('data/test_cases/', str(id))):
+				os.mkdir("%s%s" % ('data/test_cases/', str(id)))
+			else:
+				print('directory already exists')
+				return False
+			test_cases_path_rar = os.path.join('data/test_cases', str(id), str(id)+'.zip')
+			test_cases_path = os.path.join('data/test_cases', str(id))
+			f = open(test_cases_path_rar, mode="wb")
+			for i in test_cases_obj.chunks():  # TODO(tdye): using coroutine?
+				f.write(i)
+			f.close()
+			unzip_file(test_cases_path_rar, test_cases_path)
+			os.remove(test_cases_path_rar)
+			return render(request, 'upload.html', {})
+	elif request.method == 'GET':
+		if request.GET.get('action') == 'logout':
+			nextURL = request.GET.get('next')
+			request.session['checkTeacherSignin'] = False
+			request.session['teacherEmail'] = ''
+			request.session['teacherNickname'] = ''
+			return redirect(nextURL)
+
+
+def upload(request):
+	return render(request, 'upload.html', {})
+
+
+def upload_problem(request):
+	email = request.POST.get('email')
