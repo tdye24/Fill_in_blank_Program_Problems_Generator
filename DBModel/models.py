7919f6d566911762f2c1931a4dbd9df8701882dd
TDYe123 <18365225454@163.com>
2020-04-19 01:14:40 +0800

    add: (1) static methods to handle requests (2) two class Upload and Teacher

--- DBModel/models.py
+++ DBModel/models.py
@@ -11,6 +11,27 @@ class User(models.Model):
 	nickname = models.CharField(max_length=20)
 	score = models.DecimalField(max_digits=10, decimal_places=2, blank=True, default=0)
 
+	@staticmethod
+	def get_ranklist(ranklistVolume):
+		"""
+		get specified volume of ranklist
+		:param ranklistVolume: volume number of ranklist
+		:return: ranklist
+		"""
+		# TODO(tdye): need to filter some attributes of User Object like password?
+		return User.objects.all().order_by('-score')[(ranklistVolume - 1) * 20: ranklistVolume * 20]
+
+	@staticmethod
+	def get_ranklist_volumes():
+		"""
+		get how many volumes of ranklist in total
+		:return: list of volumes like [1, 2, 3]
+		"""
+		return [(i + 1) for i in range(int(User.objects.count() / 20) + 1)]
+
+	def signin(self):
+		pass    # TODO(tdye): Object Oriented
+
 
 class Problem(models.Model):
 	id = models.AutoField(primary_key=True)
@@ -21,7 +42,50 @@ class Problem(models.Model):
 	score = models.IntegerField(default=20)
 	averageScore = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 	answer = models.CharField(validators=[validate_comma_separated_integer_list], max_length=255, default="")
+	# TODO(tdye): 答案不能用逗号分开，假如答案本身就是逗号呢？
 	file = models.FileField(upload_to='data', default="")
+
+	@staticmethod
+	def get_problem_list(volume):
+		return Problem.objects.values('id', 'title', 'averageScore', 'score'). \
+			       order_by('id')[(volume - 1) * 20: volume * 20]
+
+	@staticmethod
+	def get_problem_volumes():
+		return [(i + 1) for i in range(int(Problem.objects.count() / 20) + 1)]
+
+	@staticmethod
+	def get_problem_by_id(id):
+		problem = Problem.objects.values(
+			'id', 'title', 'theme', 'author', 'description', 'answer', 'averageScore', 'score').filter(id=id)[0]
+		problem['theme'] = problem['theme'].split(',')
+		problem['blanksNums'] = [(i + 1) for i in range(len(problem['answer'].split(',')))]
+		del (problem['answer'])
+		return problem
+
+	@staticmethod
+	def get_problem_list_by_theme(theme, problemVolume):
+		"""
+		get specified them and volume problem list
+		:param theme:
+		:param problemVolume:
+		:return:
+		"""
+		return Problem.objects.filter(theme__contains=theme).values('id', 'title', 'averageScore', 'score'). \
+			       order_by('id')[(problemVolume - 1) * 20: problemVolume * 20]
+
+	@staticmethod
+	def get_problem_volumes_by_theme(theme):
+		"""
+		get specified theme problem volumes
+		:param theme:
+		:return: like [1, 2, ..., 10, ...]
+		"""
+		return [(i + 1) for i in range(int(Problem.objects.filter(theme__contains=theme).count() / 20) + 1)]
+
+	@staticmethod
+	def get_next_problem_id():
+		return (Problem.objects.values('id').order_by('-id')[0])['id'] + 1
 
 
 class Submission(models.Model):
@@ -32,3 +96,41 @@ class Submission(models.Model):
 	answer = models.CharField(validators=[validate_comma_separated_integer_list], max_length=255, default="")
 	author = models.CharField(max_length=20, default='')
 	score = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+	@staticmethod
+	def get_status_list(statusVolume):
+		"""
+		get specified volume of status list
+		:param statusVolume:
+		:return:
+		"""
+		return Submission.objects.all().order_by('-submissionId')[(statusVolume - 1) * 20: statusVolume * 20]
+
+	@staticmethod
+	def get_status_volumes():
+		"""
+		get how many volumes of status list in total
+		:return:
+		"""
+		return [(i + 1) for i in range(int(Submission.objects.count() / 20) + 1)]
+
+
+class Teacher(models.Model):
+	email = models.EmailField(null=False, primary_key=True, unique=True)
+	password = models.CharField(max_length=128)
+	nickname = models.CharField(max_length=20)
+
+	def signin(self):
+		pass
+
+	def upload(self, time, proId):
+		email = self.email
+
+
+class Upload(models.Model):
+	email = models.EmailField()
+	time = models.DateTimeField(default=timezone.now)
+	proId = models.SmallIntegerField()
+
+	class Meta:
+		unique_together = ('email', 'proId')
