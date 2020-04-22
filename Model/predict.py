5944f554c2e8f25a20e65ced1df7c90f49974743
TDYe123 <18365225454@163.com>
2020-04-22 17:13:57 +0800

    amend: (1) predict blank elements and return 3-dimensional vector format fix: (1) keras.backend.clear_session(), prevent from using the same session between two requests

--- Model/predict.py
+++ Model/predict.py
@@ -3,12 +3,16 @@
 # @date:2020/3/14 11:22
 # @name:predict
 # @author:TDYe
+import keras
 import numpy as np
-from model import build, token_vocab_size, label_vocab_size, file, id_to_label, test_labels, x_test
-from seqeval.metrics import classification_report
+from keras.preprocessing.sequence import pad_sequences
+from Model.model import build, token_vocab_size, label_vocab_size, file, id_to_label, test_labels, x_test
 
 
-def predict(x):
+def predict(X):
+	keras.backend.clear_session()
+	token_ids = [[info[2] for info in source] for source in X]
+	x = pad_sequences(token_ids, padding='post')
 	model = build(token_vocab_size, label_vocab_size, use_crf=True, token_emb_size=50, lstm_units=100)
 	model.summary()
 	model.load_weights(file)
@@ -18,10 +22,11 @@ def predict(x):
 	y = np.argmax(y, axis=-1)
 	y = [[id_to_label[ID] for ID in source if id_to_label[ID] != '<PAD>'] for source in y]
 
-	# Evaluate Model
-	print(classification_report(test_labels, y))
-	print(y)
+	for i in range(len(X)):
+		for j in range(len(X[i])):
+			X[i][j][3] = y[i][j]
+	return X
 
 
 if __name__ == '__main__':
-	predict(x_test)
+	XX = predict(x_test)
