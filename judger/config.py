08b94700295b366cbaa6e8adfdb1e564a7a0122f
TDYe123 <18365225454@163.com>
2020-05-06 09:56:25 +0800

    add: (1) moved from origin dir \judge\

---
+++ judger/config.py
@@ -0,0 +1,17 @@
+# ！usr/bin/python
+# -*- coding:utf-8 -*-#
+# @date:2020/4/3 21:57
+# @name:config
+# @author:TDYe
+count_thread = 4
+queue_size = 4
+db_host = "localhost"
+db_port = '3366'
+db_user = "root"
+db_password = '158728'
+db_name = 'elp'
+db_charset = 'utf8'
+server_port = '9906'
+work_dir = 'work_dir'
+data_dir = 'data_dir'
+auto_clean = True
