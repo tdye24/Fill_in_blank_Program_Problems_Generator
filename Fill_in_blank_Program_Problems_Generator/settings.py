f7a4b2339c9eb3f998893c7c7e5b3e7f96223f12
TDYe123 <18365225454@163.com>
2020-05-06 09:15:45 +0800

    amend: (1) amend TIME_ZONE = 'Asia/Shanghai'

--- Fill_in_blank_Program_Problems_Generator/settings.py
+++ Fill_in_blank_Program_Problems_Generator/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'DBModel',
+    'judger',
 ]
 
 MIDDLEWARE = [
@@ -110,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
